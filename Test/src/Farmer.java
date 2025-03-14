4d4b44e59544a1696916fb4588d69818e030d02f
rehankiw <rehanamrllh@gmail.com>
2025-03-14 13:08:52 +0700

    Update Test

---
+++ Test/src/Farmer.java
@@ -0,0 +1,9 @@
+public class Farmer {
+    String name, favourite;
+
+    public void talk(){
+        System.out.println("Hi my name is: " + name + ". My favourite plant is: " + favourite);
+        
+    }
+    
+}
