1a4b827b329d490ff6c47bfe95a360de8c936063
rehanamrllh <rehanamrllh@gmail.com>
2025-04-13 00:03:04 +0700

    Fix typo in Farmer class talk method

--- Test/src/Farmer.java
+++ Test/src/Farmer.java
@@ -2,7 +2,7 @@ public class Farmer {
     String name, favourite;
 
     public void talk(){
-        System.out.println("Hi my name is: " + name + ". My favourite plant is: " + favourite);
+        System.out.println("Hi mi name is: " + name + ". My favourite plant is: " + favourite);
         
     }
     
