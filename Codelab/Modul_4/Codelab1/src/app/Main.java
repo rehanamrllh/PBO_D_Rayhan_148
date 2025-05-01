ca4f8a02eafb731fbadacb67a27f275efac9c0f0
rehanamrllh <rehanamrllh@gmail.com>
2025-05-01 19:45:26 +0700

    Add initial implementation of library management system with book borrowing features

---
+++ Codelab/Modul_4/Codelab1/src/app/Main.java
@@ -0,0 +1,25 @@
+package app;
+
+import perpustakaan.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Buku buku1 = new NonFiksi("Madilog", "Tan Malaka", "Sejarah & Ilmu Pengetahuan");
+        Buku buku2 = new Fiksi("Hainuwele: Sang Putri Kelapa", "Lilis Hu", "Dongeng");
+
+        buku1.displayInfo();
+        buku2.displayInfo();
+
+        Anggota anggota1 = new Anggota("Rayhan Amrullah", "148");
+        Anggota anggota2 = new Anggota("Daffa Ichsanudin", "158");
+
+        anggota1.displayInfo();
+        anggota2.displayInfo();
+
+        anggota1.pinjamBuku("Madilog");
+        anggota2.pinjamBuku("Hainuwele: Sang Putri Kelapa", 7);
+
+        anggota1.kembalikanBuku("Madilog");
+        anggota2.kembalikanBuku("Hainuwele: Sang Putri Kelapa");
+    }
+}
